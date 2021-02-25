23bc04eaf448525fa232a0157171d3e54ff4d0b5
Lazaraaus <wood.yjaden@gmail.com>
2021-02-25 12:48:49 -0500

    Initial Commit, stock management app

---
+++ c++/utils.cpp
@@ -0,0 +1,18 @@
+#include "utils.h"
+
+template<typename Out>
+void split(const string &s, char delimiter, Out result) {
+    stringstream ss(s);
+    string item;
+    while (std::getline(ss, item, delimiter)) {
+        *(result++) = item;
+    }
+}
+
+vector<string> split(const std::string &s, char delimiter) {
+    vector<string> elements;
+    split(s, delimiter, std::back_inserter(elements));
+    return elements;
+}
+
+
